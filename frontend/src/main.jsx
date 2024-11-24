68e7cd0300e4771e2aaa3aca2e0967a11c3d5baf
manish_singh <manishkandari43@gmail.com>
2024-11-24 17:59:00 +0530

    coding

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // Importing global CSS file
-import App from './App';  // Importing the App component
-
+import './index.css';
+import App from './App'; 
+import './App.css'; 
 const rootElement = document.getElementById('root');  // Find the div with id="root" in index.html
 const root = ReactDOM.createRoot(rootElement);  // Create a root for React
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "face-api.js": "face-api.js/dist/face-api.js", // सुनिश्चित करें कि सही पथ है
-    },
-  },
   plugins: [react()],
 });
